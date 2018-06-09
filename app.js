651e54cfdf2b2fc6d049acc5c195f21d789cf067
Allen Welch <allenjwelch@gmail.com>
2018-06-09 10:03:41 -0400

    added new card function, issue with append method not adding

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
+let cardInfo = {
+  style: 'style', 
+  abv: 5.5, 
+  char: 'Sample'
+}; 
+
+
+
 
 $('.deckSelection').on('click', '.decks', function() {
   let newDeck = $(this); 
   let cardContainer = newDeck[0].childNodes[1]; 
   console.log(newDeck); 
   console.log(newDeck[0].childNodes[1]); 
-  $('.deckActive').append(newDeck)
-  newDeck.addClass('appear'); 
-  cardContainer.addClass('flip-active');
+  addCard(cardInfo); 
+  // $('.deckActive').append(newDeck)
+  // newDeck.addClass('appear'); 
 })
 
 $('.deckActive').on('click', '.decks', function() {
@@ -16,7 +24,22 @@ $('.deckActive').on('click', '.decks', function() {
   oldDeck.addClass('appear');
 })
 
-
+function addCard(cardInfo) {
+  let container = `<div class="card-container appear">`; 
+  let card = `<div class="card" id="d1c1">`; 
+  let front = `<div class="front">`; 
+    let frontInfo = `<h3 id="${cardInfo.style}">${cardInfo.style}</h3>`;
+  let back = `<div class="back">`;
+    let backInfo = `<p>ABV: ${cardInfo.abv}\n Characteristics: ${cardInfo.char}`; 
+    let links = `<a href="#">Nailed it!</a>`; 
+  $(front).append(frontInfo); 
+  $(back).append(backInfo, links); 
+  $(card).append(front, back); 
+  console.log(card); 
+  $(container).append(card);
+  console.log(container); 
+  $('.deckActive').append(container); 
+}
 
 
 // https://css-tricks.com/almanac/properties/a/animation/
