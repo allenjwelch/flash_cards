d74307bc5e93fd9f21b74e51ff3ff70df89c0de7
Allen Welch <allenjwelch@gmail.com>
2018-06-12 18:31:29 -0400

    cardBtns move through array of cards in order

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // TODO: create array of card objects for sample data
 
-let cardInfo = [
+let deckInfo = [
   {
     style: 'style1', 
     abv: 5.5, 
@@ -16,6 +16,8 @@ let cardInfo = [
   },
 ]; 
 
+let cardPos = 0; 
+
 
 
 
@@ -24,7 +26,7 @@ $('.deckSelection').on('click', '.decks', function() {
   let cardContainer = newDeck[0].childNodes[1]; 
   console.log(newDeck); 
   // console.log(newDeck[0].childNodes[1]); 
-  addCard(cardInfo); 
+  activeCards(deckInfo); 
   // $('.deckActive').append(newDeck)
   newDeck.addClass('disappear'); 
 })
@@ -38,26 +40,52 @@ $('.deckActive').on('click', '.decks', function() {
 
 // TODO: have sample data load cards to be added to active div after click from deckSelection
 
-function addCard(cardInfo) {
-  let container = $(`<div class="card-container appear">`); 
-  let card = $(`<div class="card" id="d1c1">`); 
-  let front = $(`<div class="front">`); 
-    let frontInfo = $(`<h3 id="${cardInfo.style}">Style: ${cardInfo.style}</h3>`);
-  let back = $(`<div class="back">`);
-    let backInfo = $(`<p>ABV: ${cardInfo.abv}\n Characteristics: ${cardInfo.char}</p>`); 
-    let links = $(`<a href="#">Nailed it!</a>`); 
-  front.append(frontInfo); 
-  back.append(backInfo, links); 
-  card.append(front, back); 
-    console.log(card); 
-  container.append(card);
-    console.log(container); 
-  $('.deckActive').append(container); 
+function activeCards(deckInfo) {
+  let cardArr = [];
+  cardPos = 0; 
+  $('.deckActive').empty(); 
+  deckInfo.forEach((cardInfo) => {
+    let container = $(`<div class="card-container appear">`); 
+    let card = $(`<div class="card" id="d1c1">`); 
+    let front = $(`<div class="front">`); 
+      let frontInfo = $(`<h3 id="${cardInfo.style}">Style: ${cardInfo.style}</h3>`);
+    let back = $(`<div class="back">`);
+      let backInfo = $(`<p>ABV: ${cardInfo.abv}\n Characteristics: ${cardInfo.char}</p>`); 
+      let links = $(`<a class="cardBtns" href="#">Nailed it!</a><a class="cardBtns" href="#">Kinda</a><a class="cardBtns" href="#">Not at all</a>`); 
+    front.append(frontInfo); 
+    back.append(backInfo, links); 
+    card.append(front, back); 
+      console.log(card); 
+    container.append(card);
+      console.log(container); 
+    // $('.deckActive').append(container); 
+
+    // push cards into an array ====
+    cardArr.push(container); 
+      console.log(cardArr); 
+  })
+
+  $('.deckActive').html(cardArr[cardPos]); 
+
+  // event listener for moving through cards in active deck ==
+  $('.deckActive').on('click', '.cardBtns', function() {
+    console.log('clicked')
+    console.log(cardPos)
+    cardPos++; 
+    if (cardPos > (cardArr.length - 1)) {
+      cardPos = 0
+    }
+    console.log(cardPos)
+    $('.card-container').addClass('disappear');
+    setTimeout(function() {
+      $('.card-container').removeClass('disappear');
+      $('.deckActive').html(cardArr[cardPos]); 
+    }, 500);
+  }); 
+  
 }
 
-// TODO: create forEach for all cards
 
-// TODO: push cards into an array
 
 // TODO: card links (nailed it, etc) should move user to the next card
 
